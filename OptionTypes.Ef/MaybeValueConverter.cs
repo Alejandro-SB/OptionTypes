7d5b980075592a54327b2226c8a938efd64b6534
Alejandro <alejandro.sanchez.bastida@gmail.com>
2023-01-24 20:42:29 +0100

    Add EF ValueConverter

---
+++ OptionTypes.Ef/MaybeValueConverter.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace OptionTypes.Ef;
+
+public class MaybeValueConverter<T> : ValueConverter<Maybe<T>, T>
+{
+    public MaybeValueConverter() 
+        : base(v => v.ValueOr(null!), v => Maybe.FromValue(v))
+    {
+    }
+}
