1df4cb8ea48565363ae60f305411bd0f593eb3eb
Alejandro Sanchez Bastida <alejandro.sanchez.bastida@gmail.com>
2025-06-15 17:07:32 +0200

    Add source generator project

---
+++ Funzo.SourceGenerators/ResultDiagnosticDescriptors.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Funzo;
+internal static class ResultDiagnosticDescriptors
+{
+    public static DiagnosticDescriptor WrongBaseType
+        => new("FNZ0001", "Result must inherit from Result",
+            "Class '{0}' should inherit from Result",
+            "ResultGenerator",
+            DiagnosticSeverity.Error,
+            true);
+}
